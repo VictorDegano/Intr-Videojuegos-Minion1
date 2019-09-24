2dcb4938b29b8e0ecadffeb8240c2250a0155648
VictorDegano <flame.el22@gmail.com>
2019-09-24 20:12:44 -0300

    Agregado fondo de menu y musica de juego
    Se agrego un background para el menu de incio y tambien se agrego una musica de fondo para el juego.

--- Interfaz.gd
+++ Interfaz.gd
@@ -3,6 +3,8 @@ extends CanvasLayer
 onready var messageLabel = $Mensajes
 onready var messageTimer = $Timer
 onready var startbutton = $Button
+onready var background = $ParallaxBackground
+var firstPlay = true
 
 func _ready():
 	Event.connect("game_over",self,"game_over")
@@ -17,6 +19,7 @@ func show_message(message):
 func game_over():
 	show_message("¡Game Over!")
 	yield(messageTimer, "timeout")
+	yield(get_tree().create_timer(0.5), "timeout")
 	startbutton.show()
 	messageLabel.text = "Bunny Jump"
 	messageLabel.show()
@@ -27,7 +30,11 @@ func _on_timeout():
 	messageLabel.hide()
 
 func _on_button_pressed():
-#	get_tree().set_pause(false)
 	messageLabel.hide()
 	startbutton.hide()
+	
+	if firstPlay:
+		background.free()
+		firstPlay = false
+		
 	Event.emit_signal("start_game")
--- Scenes/Game.gd
+++ Scenes/Game.gd
@@ -1,2 +1,8 @@
 extends Node
 
+func _ready():
+	Event.connect("game_over",self,"_on_game_over")
+	
+func _on_game_over():
+	$BackgroundMusicAudio/Tween.interpolate_property($BackgroundMusicAudio, "volume_db", -3, -80, 10, Tween.TRANS_LINEAR, Tween.EASE_IN, 0)
+	$BackgroundMusicAudio/Tween.start()
